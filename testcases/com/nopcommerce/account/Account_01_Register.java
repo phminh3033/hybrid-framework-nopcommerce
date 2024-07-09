8f65de55165d55c6ddedd912e694ab662c1dd443
phminh3033 <phminh3033@gmail.com>
2024-07-09 22:27:31 +0700

    Topic 52 Build Automation Framework Structure

---
+++ testcases/com/nopcommerce/account/Account_01_Register.java
@@ -0,0 +1,28 @@
+package com.nopcommerce.account;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Account_01_Register {
+    @BeforeClass
+    public void beforeClass() {
+        // Setup code for registering a new account
+    }
+
+    @Test
+    public void Register_01_Empty_data() {
+        // Test case for registering a new account
+    }
+
+    @Test
+    public void Register_05_Success() {
+        // Test case for registering a new account
+    }
+
+
+    @AfterClass
+    public void afterClass() {
+        // Cleanup code for deleting the registered account
+    }
+}
